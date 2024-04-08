fe77f7c67c75b2d54879ba07e240ac10159bcf37
SachitDesai <sachitdesai11@gmail.com>
2024-04-08 10:44:28 +0530

    made changes to news page. work still in progress

--- src/newsContent.js
+++ src/newsContent.js
@@ -11,6 +11,7 @@ export const NEWSCONTENT = [
         // desc: "The Reds suffered stoppage-time heartbreak to lose 2-1 at Tottenham after some questionable decisions by the officials.",
         image: news1,
         link: 'https://www.goal.com/en-in/lists/joel-matip-winners-losers-nine-man-liverpool-blow-it-klopp-var/blt1d50c084b7891c7b',
+        desc: "Liverpool arrived at the Tottenham Hotspur Stadium having never lost at Spurs' still relatively-new home. In fact, they were the only Premier League side that not yet tasted defeat at the impressive arena. And as Saturday's match entered the 96th minute, Jurgen Klopp must have thought that his team were about to maintain that record, despite everything that had been thrown at them over the course of the evening."
     },
     {
         id: 2,
@@ -18,13 +19,15 @@ export const NEWSCONTENT = [
         // desc: "Man Utd player ratings vs Crystal Palace: Marcus Rashford goes missing yet again as rudderless Red Devils go right back to being useless",
         image: news2, 
         link: 'https://www.goal.com/en-in/lists/man-utd-player-ratings-vs-crystal-palace-marcus-rashford-rasmus-hojlund/bltba89b3f4314a691c',
+        desc: "So much for Manchester United's revival. After a slender win at Burnley and a convincing Carabao Cup victory over Crystal Palace, Erik ten Hag's side are right back to square one after a limp 1-0 defeat at home to Palace. A deflected Joachim Andersen goal in the first half proved the difference but what was most concerning was United's severe lack of attacking spark. And the blame must fall on Marcus Rashford, who had a really poor game once more and could be in danger of losing his place to Alejandro Garnacho if he keeps this dire form up."
     },
     {
         id: 3,
         heading: 'Man City player ratings vs Wolves',
         // desc: 'Erling Haaland anonymous as dodgy defending costs champions in first Premier League loss of the season',
         image: news3, 
-        link: 'https://www.goal.com/en-in/lists/man-city-player-ratings-vs-wolves-erling-haaland-julian-alvarez/blte93c876980382439'
+        link: 'https://www.goal.com/en-in/lists/man-city-player-ratings-vs-wolves-erling-haaland-julian-alvarez/blte93c876980382439',
+        desc: "Manchester City's perfect start to the Premier League season was brought to an end as they were beaten 2-1 by a solid Wolves team on Saturday. Pep Guardiola will certainly not forget the name of Hwang Hee-chan again after the forward netted the winning goal just a day after the City boss labelled him 'the Korean guy' in his press conference."
     },
     {
         id: 4,
--- src/pages/news/newsTemplate.jsx
+++ src/pages/news/newsTemplate.jsx
@@ -1,9 +1,12 @@
 //Newstemplate.jsx
 import React from 'react'
+import './newsTemplate.css'
+
 export const NewsTemplate = (props) => {
     const { heading, desc, image, link } = props.data;
   return (
-    <a href={link} target="_blank"><div className="newsBox">
+    <a href={link} target="_blank">
+      {/* <div className="newsBox">
       <img src={image} alt='newsImage'/>
       <div className="description">
         <p className='heading'>
@@ -11,6 +14,23 @@ export const NewsTemplate = (props) => {
         </p>
         <p>{desc}</p>
       </div>
-    </div></a>
+    </div> */}
+      <div class="container">
+  <div class="text-center">
+  </div>
+  <div class="container">
+    <div class="card-columns">
+      <div class="card">
+        <a href="#" />
+        <img class="card-img-top" src={image} alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">{heading}</h5>
+          <p class="card-text">{desc}</p>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+    </a>
   )
 }
